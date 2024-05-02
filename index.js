f0cd0e6154ef81b91800b42426d26559e7ed9b7d
Whitney Monique <wm@WMs-MacBook-Air-8.local>
2024-05-01 21:23:25 -0700

    done

---
+++ index.js
@@ -0,0 +1,40 @@
+
+// intro message
+
+const greeting = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:'
+
+console.log(greeting)
+
+/* combination is 10 - 40 - 39
+
+num1 is subtraction, difference 10
+num2 is division, quotient 40
+num3 is addition, sum 39
+*/
+
+
+let Num1 = 100 - 90
+let Num2 = 80 / 2
+let Num3 = 38 + 1
+
+/*console.log for final numbers*/
+
+console.log(Num1)
+console.log(Num2)
+console.log(Num3)
+
+/*
+const for sequence
+*/
+const combo = '${Num1} - ${Num2} - ${Num3}'
+
+/*alert showing the combination*/
+
+alert("CONGRATS! COMBINATION IS 10-40-39")
+
+
+
+
+const name = "10 - 40 -39"
+
+console.log(You)
